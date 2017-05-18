cc7e80f25d5995583ddeba23b4bd86a8df68288f
manulul <eaaemnie1@students.eaaa.dk>
2017-05-18 13:56:03 +0200

    Coding startup
    Added the index file, added stylesheet, added slider for index

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <title>Trygsen Service</title>
+
+       	<link rel="stylesheet" type="text/css" href="css/style.sass">
+       	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+</head>
+
+<body>
+
+		<main>
+				<header>
+
+						<nav>
+								<img src="images/logo.png" alt="trygsen service logo">	
+									<ul>
+										<li>Produkter</li>
+										<li>Kontakt</li>
+										<li>Galleri</li>
+										<li>Generelle vilkår</li>
+										<li>Om os</li>
+									</ul>
+						</nav>
+
+				</header>
+
+
+				<section>
+							
+
+
+				</section>
+		</main>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
